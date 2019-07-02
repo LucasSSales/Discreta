16025aea16b89029a3b9a7250012f240730d32f4
Lucas Sales <ldss@ic.ufal.br>
2019-07-02 18:30:55 -0300

    Um jeito mais simples

--- questao3.py
+++ questao3.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
-n = int(input('kk eae men, digita:'))
+n = int(input('Digita:'))
 n2 = n
 x = 2
+bases = []
+exp = {}
 
 while x!=n:
 	if(n2%x==0):
+		if x not in bases:
+			bases.append(x)
+		if x not in exp:
+			exp[x] = 1
+		else:
+			exp[x] += 1
 		print(""+str(x))
 		n2 = n2//x
 	if(n2%x!=0):
 		x+=1
+
+print(bases)
+print(exp)
